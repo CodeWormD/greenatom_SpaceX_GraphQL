7a83e715a58f03a65e5739b38ea35b86b438661d
Daniil <dan.dad@bk.ru>
2023-02-15 17:16:59 +0300

    new2

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
 import sys
 import subprocess
 from time import sleep
+import os
 
-x = subprocess.Popen(['python', '-m', 'venv', 'venv'])
-x.wait()
-x = subprocess.Popen([sys.executable, 'source venv/Scripts/activate'])
-x.wait()
-x = subprocess.Popen([sys.executable, 'python.exe -m pip install --upgrade pip'])
+
+
+# cmd = '/venv/Scripts/activate'
+
+# x = subprocess.Popen(['python', '-m', 'venv', 'venv'])
+# x.wait()
+# # x = subprocess.Popen(['source /venv/Scripts/activate'])
+# # x.wait()
+x = subprocess.Popen(['python.exe -m pip install --upgrade pip'])
 x.wait()
 x = subprocess.Popen(['pip', 'install', '-r', 'req.txt'])
 x.wait()
